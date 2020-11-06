4d0b156e64b4f04ae02187e8c05ba93fdcb07c5a
Pratik Thanki <pratikthanki1@gmail.com>
2020-11-06 23:48:16 +0000

    Update games

--- FourthDown.Collector/Program.cs
+++ FourthDown.Collector/Program.cs
@@ -1,6 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using FourthDown.Collector.Configuration;
 using FourthDown.Collector.Repositories;
 using FourthDown.Collector.Repositories.Csv;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,14 +13,27 @@ namespace FourthDown.Collector
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        static async Task Main(string[] args)
         {
             var builder = new HostBuilder()
                 .ConfigureServices((hostContext, services) =>
                 {
+                    services.Configure<Options>(hostContext.Configuration);
                     services.AddHostedService<DataCollector>();
                     services.AddSingleton<IPlayByPlayRepository, CsvPlayByPlayRepository>();
                     services.AddSingleton<IGameRepository, CsvGameRepository>();
+                })
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    var env = hostingContext.HostingEnvironment;
+
+                    configuration
+                        .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                        .AddJsonFile("appsettings.json");
+
+                    var configurationRoot = configuration.Build();
                 });
 
             await builder.RunConsoleAsync();
--- FourthDown.Collector/Repositories/Csv/CsvGameRepository.cs
+++ FourthDown.Collector/Repositories/Csv/CsvGameRepository.cs
@@ -6,29 +6,46 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using FourthDown.Shared.Models;
+using Microsoft.Extensions.Options;
+using Options = FourthDown.Collector.Configuration.Options;
 
 namespace FourthDown.Collector.Repositories.Csv
 {
     public class CsvGameRepository : IGameRepository
     {
+        private readonly Options _options;
+        private readonly int FirstSeason = 1999;
+        private const string url = @"https://raw.githubusercontent.com/leesharpe/nfldata/master/data/games.csv";
+
+        public CsvGameRepository(IOptions<Options> options)
+        {
+            _options = options.Value;
+        }
+
         public async Task<IEnumerable<Game>> GetGames(int season, CancellationToken cancellationToken)
         {
             var currentSeason = CurrentSeason();
-            const string url = @"https://raw.githubusercontent.com/leesharpe/nfldata/master/data/games.csv";
-            var timeout = TimeSpan.FromSeconds(10);
+            var seasons = _options.BackLoad
+                ? Enumerable.Range(FirstSeason, currentSeason - FirstSeason)
+                : new List<int>() {currentSeason};
 
+            var timeout = TimeSpan.FromSeconds(10);
             var httpClientHandler = new HttpClientHandler
             {
                 AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
             };
 
             var httpClient = new HttpClient(httpClientHandler) {Timeout = timeout};
-            var response = await httpClient.GetAsync(url, cancellationToken);
 
+            var response = await httpClient.GetAsync(url, cancellationToken);
             var responseBody = await response.Content.ReadAsStringAsync();
-            var games = responseBody.Split("\n").Skip(1);
-            
-            return games.Select(x => new Game(x));
+
+            var games = responseBody.Split("\n").Skip(1)
+                .Select(x => new Game(x))
+                .Where(x => seasons.Contains(x.Season))
+                .ToList();
+
+            return games;
         }
 
         private static int CurrentSeason()
