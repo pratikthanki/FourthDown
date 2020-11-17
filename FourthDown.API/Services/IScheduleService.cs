6e101ae28b5c52baf913129b9948d42193411220
Pratik Thanki <pratikthanki1@gmail.com>
2020-11-17 16:57:20 +0000

    Schedule service

--- FourthDown.API/Services/IScheduleService.cs
+++ FourthDown.API/Services/IScheduleService.cs
@@ -2,12 +2,12 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using FourthDown.Api.Models;
+using FourthDown.Api.Parameters;
 
 namespace FourthDown.Api.Services
 {
     public interface IScheduleService
     {
-        Task<IEnumerable<Game>> GetGamesForWeek(int week, CancellationToken cancellationToken);
-        Task<IEnumerable<Game>> GetGamesForCurrentWeek(CancellationToken cancellationToken);
+        Task<IEnumerable<Game>> GetGames(ScheduleQueryParameter queryParameter, CancellationToken cancellationToken);
     }
 }
--- FourthDown.API/Services/ScheduleService.cs
+++ FourthDown.API/Services/ScheduleService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FourthDown.Api.Models;
+using FourthDown.Api.Parameters;
 using FourthDown.Api.Repositories;
 
 namespace FourthDown.Api.Services
@@ -23,25 +24,27 @@ namespace FourthDown.Api.Services
 
         private int CurrentSeason() => Today.Month > 8 ? Today.Year : Today.Year - 1;
 
-        private int GetCurrentWeek(IEnumerable<Game> games) =>
-            games
+        private int GetCurrentWeek(IEnumerable<Game> games)
+        {
+            return games
                 .Where(x => x.Season == _currentSeason && x.Gameday <= Today.Date)
                 .Max(x => x.Week);
-
-        public async Task<IEnumerable<Game>> GetGamesForWeek(int week, CancellationToken cancellationToken)
-        {
-            var games = await _gameRepository.GetGames(cancellationToken);
-            
-            return games.Where(x => x.Season == _currentSeason && x.Week == week);
         }
-        
-        public async Task<IEnumerable<Game>> GetGamesForCurrentWeek(CancellationToken cancellationToken)
+
+        public async Task<IEnumerable<Game>> GetGames(
+            ScheduleQueryParameter queryParameter,
+            CancellationToken cancellationToken)
         {
             var games = await _gameRepository.GetGames(cancellationToken);
-            
+
             var currentWeek = GetCurrentWeek(games);
-            
-            return games.Where(x => x.Season == _currentSeason && x.Week == currentWeek);
+
+            var week = queryParameter.Week ?? currentWeek;
+            var season = queryParameter.Season ?? _currentSeason;
+            var team = queryParameter.Team;
+
+
+            return games.Where(x => x.Season == _currentSeason && x.Week == week);
         }
     }
 }
