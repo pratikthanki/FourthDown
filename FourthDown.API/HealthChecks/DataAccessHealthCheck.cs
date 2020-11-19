be0415b3f0cf34030a3ba6d2a8683fefb86976b3
Pratik Thanki <pratikthanki1@gmail.com>
2020-11-19 23:34:50 +0000

    add health check

---
+++ FourthDown.API/HealthChecks/DataAccessHealthCheck.cs
@@ -0,0 +1,30 @@
+using System.Threading;
+using System.Threading.Tasks;
+using FourthDown.Api.Utilities;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace FourthDown.Api.HealthChecks
+{
+    public class DataAccessHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            const string url =
+                @"https://github.com/pratikthanki/nflfastR-raw/blob/master/raw/2020/2020_01_DAL_LA.json.gz?raw=true";
+
+            var response = await RequestHelper.GetRequestResponse(url, cancellationToken);
+            var healthCheckResultHealthy = response.IsSuccessStatusCode;
+
+            if (healthCheckResultHealthy)
+            {
+                return await Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return await Task.FromResult(
+                HealthCheckResult.Unhealthy("An unhealthy result."));
+        }
+    }
+}
