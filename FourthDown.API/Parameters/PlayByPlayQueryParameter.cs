2e513196b3ef4b72978b2539323f5b06d948f14f
Pratik Thanki <pratikthanki1@gmail.com>
2020-11-19 23:33:30 +0000

    annotate params

--- FourthDown.API/Parameters/PlayByPlayQueryParameter.cs
+++ FourthDown.API/Parameters/PlayByPlayQueryParameter.cs
@@ -1,16 +1,37 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FourthDown.Api.Parameters
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class PlayByPlayQueryParameter
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public int? Week { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int? Season { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string Team { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string GameId { get; set; }
 
         private bool NonGameIdParameterSet() => Week != null || Season != null || Team != null;
 
+#pragma warning disable 1591
         public Dictionary<string, string[]> Validate()
         {
             var errors = new Dictionary<string, string[]>();
@@ -28,5 +49,6 @@ namespace FourthDown.Api.Parameters
 
             return errors;
         }
+#pragma warning restore 1591
     }
 }
--- FourthDown.API/Parameters/ScheduleQueryParameter.cs
+++ FourthDown.API/Parameters/ScheduleQueryParameter.cs
@@ -1,11 +1,30 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace FourthDown.Api.Parameters
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ScheduleQueryParameter
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public int? Week { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int? Season { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string Team { get; set; }
 
+#pragma warning disable 1591
         public bool IsNull() => Week == null && Season == null && string.IsNullOrWhiteSpace(Team);
+#pragma warning restore 1591
     }
 }
