4aaa21d8904a5b487b8280f2a21ace09b3661d41
Pratik Thanki <pratikthanki1@gmail.com>
2020-11-21 00:08:10 +0000

    Update from gzip stream

--- FourthDown.API/Repositories/Json/JsonPlayByPlayRepository.cs
+++ FourthDown.API/Repositories/Json/JsonPlayByPlayRepository.cs
@@ -6,14 +6,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using FourthDown.Api.Models;
 using FourthDown.Api.Utilities;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.GZip;
+using Microsoft.Extensions.Logging;
 
 namespace FourthDown.Api.Repositories.Json
 {
     public class JsonPlayByPlayRepository : IPlayByPlayRepository
     {
-        public JsonPlayByPlayRepository()
+        private static ILogger<JsonPlayByPlayRepository> _logger;
+        
+        public JsonPlayByPlayRepository(ILogger<JsonPlayByPlayRepository> logger)
         {
-            
+            _logger = logger;
         }
 
         private string GetGameUrl(string gameId, int season) =>
@@ -34,28 +39,37 @@ namespace FourthDown.Api.Repositories.Json
             var response = await RequestHelper.GetRequestResponse(url, cancellationToken);
             var stream = await response.Content.ReadAsStreamAsync();
 
-            string data;
-            await using (var gs = new GZipStream(stream, CompressionMode.Decompress))
-            await using (var mso = new MemoryStream())
-            {
-                var bytes = new byte[4096];
-                int cnt;
-                while ((cnt = await gs.ReadAsync(bytes, 0, bytes.Length, cancellationToken)) != 0)
-                {
-                    await mso.WriteAsync(bytes, 0, cnt, cancellationToken);
-                }
-
-                data = Encoding.UTF8.GetString(mso.ToArray());
-            }
+            var data = await ReadCompressedStreamToString(stream);
 
             var jsonSerializerOptions = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             };
-            
-            var gameRaw = JsonSerializer.Deserialize<GameRaw>(data, jsonSerializerOptions);
+
+            GameRaw gameRaw = null;
+            try
+            {
+                gameRaw = JsonSerializer.Deserialize<GameRaw>(data, jsonSerializerOptions);
+            }
+            catch (JsonException JsonException)
+            {
+                _logger.LogError($"Error in deserializing json string: {JsonException}\n url: {url}");
+            }
 
             return gameRaw.Data.Viewer.GameDetail;
+        }
+
+        private static async Task<string> ReadCompressedStreamToString(Stream stream)
+        {
+            await using var inStream = new GZipInputStream(stream);
+            await using var MemoryStream = new MemoryStream();
+
+            byte[] buffer = new byte[4096];
+            StreamUtils.Copy(inStream, MemoryStream, buffer);
+            
+            var data = Encoding.UTF8.GetString(MemoryStream.ToArray());
+
+            return data;
         }
     }
 }
