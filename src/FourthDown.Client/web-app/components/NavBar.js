abf3814df178fdb7a9cb4a41cc9a923b2eaf0dab
Sharoon <sharoonarif@gmail.com>
2021-02-07 11:15:27 +0000

    Switch to built in sass plugin

--- src/FourthDown.Client/web-app/components/NavBar.js
+++ src/FourthDown.Client/web-app/components/NavBar.js
@@ -1,8 +1,8 @@
-import "./NavBar.scss";
 import NavButton from "./NavButton";
+import styles from "./NavBar.module.scss";
 
 const NavBar = props => (
-  <div className="NavBar">
+  <div className={styles.NavBar}>
     {props.navButtons.map(button => (
       <NavButton
         key={button.path}
