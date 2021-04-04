4aeb9f42b9e22263c16f890cd39a3a738910f3c4
Pratik Thanki <pratikthanki1@gmail.com>
2021-04-05 00:15:31 +0100

    Upate db options

--- src/FourthDown.Collector/Options/DatabaseOptions.cs
+++ src/FourthDown.Collector/Options/DatabaseOptions.cs
@@ -2,21 +2,6 @@ namespace FourthDown.Collector.Options
 {
     public class DatabaseOptions
     {
-        public string Server { get; set; }
-
-        public string Database { get; set; }
-
-        public string UserId { get; set; }
-
-        public string Password { get; set; }
-
-        public string ConnectionString => ConnectionStringBuilder(Database);
-
-        public string MasterConnectionString => ConnectionStringBuilder("master");
-
-        private string ConnectionStringBuilder(string db) =>
-            $"Server={Server};Database={db};User Id={UserId};Password='{Password}'";
-
-        public override string ToString() => ConnectionString;
+        public string ConnectionString { get; set; }
     }
 }
