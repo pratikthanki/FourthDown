be25e22cf457289da135d96a6033e83ac120204c
Pratik Thanki <pratik@Pratiks-MacBook-Pro.local>
2023-11-26 22:38:10 +0000

    More minor fixes and reduce compile time warnings

--- src/FourthDown.Shared/Models/GameDetail.cs
+++ src/FourthDown.Shared/Models/GameDetail.cs
@@ -1,5 +1,7 @@
+#nullable enable
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using JetBrains.Annotations;
 
 namespace FourthDown.Shared.Models
 {
@@ -8,59 +10,71 @@ namespace FourthDown.Shared.Models
         public DataObject Data { get; set; }
     }
 
+    [UsedImplicitly]
     internal class DataObject
     {
         public Viewer Viewer { get; set; }
     }
 
+    [UsedImplicitly]
     internal class Viewer
     {
         public GameDetail GameDetail { get; set; }
     }
 
+    [UsedImplicitly]
     public class HomePointsOvertime
     {
     }
 
+    [UsedImplicitly]
     public class GameTeam
     {
         public IReadOnlyList<string> Abbreviation { get; set; }
         public IReadOnlyList<string> NickName { get; set; }
     }
 
+    [UsedImplicitly]
     public class Period
     {
     }
 
+    [UsedImplicitly]
     public class ScoringSummary
     {
         public int PlayId { get; set; }
-        public string PlayDescription { get; set; }
+        public string? PlayDescription { get; set; }
         public int PatPlayId { get; set; }
         public int HomeScore { get; set; }
         public int VisitorScore { get; set; }
     }
 
+    [UsedImplicitly]
     public class VisitorPointsOvertime
     {
     }
 
+    [UsedImplicitly]
     public class CurrentFahrenheit
     {
     }
 
+    [UsedImplicitly]
     public class Location
     {
     }
 
+    [UsedImplicitly]
     public class LongDescription
     {
     }
 
+    [UsedImplicitly]
     public class CurrentRealFeelFahrenheit
     {
     }
 
+    [UsedImplicitly]
     public class Weather
     {
         public CurrentFahrenheit CurrentFahrenheit { get; set; }
@@ -70,6 +84,7 @@ namespace FourthDown.Shared.Models
         public CurrentRealFeelFahrenheit CurrentRealFeelFahrenheit { get; set; }
     }
 
+    [UsedImplicitly]
     public class YardLine
     {
     }
@@ -77,14 +92,14 @@ namespace FourthDown.Shared.Models
     public class Drive
     {
         public int QuarterStart { get; set; }
-        public string EndTransition { get; set; }
-        public string EndYardLine { get; set; }
+        public string? EndTransition { get; set; }
+        public string? EndYardLine { get; set; }
         public bool EndedWithScore { get; set; }
         public int FirstDowns { get; set; }
-        public string GameClockEnd { get; set; }
-        public string GameClockStart { get; set; }
-        public string HowEndedDescription { get; set; }
-        public string HowStartedDescription { get; set; }
+        public string? GameClockEnd { get; set; }
+        public string? GameClockStart { get; set; }
+        public string? HowEndedDescription { get; set; }
+        public string? HowStartedDescription { get; set; }
         public bool Inside20 { get; set; }
         public double OrderSequence { get; set; }
         public int PlayCount { get; set; }
@@ -93,67 +108,69 @@ namespace FourthDown.Shared.Models
         public int PlaySeqEnded { get; set; }
         public int PlaySeqStarted { get; set; }
         public int QuarterEnd { get; set; }
-        public string StartTransition { get; set; }
-        public string StartYardLine { get; set; }
-        public string TimeOfPossession { get; set; }
+        public string? StartTransition { get; set; }
+        public string? StartYardLine { get; set; }
+        public string? TimeOfPossession { get; set; }
         public int Yards { get; set; }
         public int YardsPenalized { get; set; }
-        public string PossessionTeamAbbreviation { get; set; }
-        public string PossessionTeamNickName { get; set; }
-        public string PossessionTeamFranchiseCurrentLogoUrl { get; set; }
+        public string? PossessionTeamAbbreviation { get; set; }
+        public string? PossessionTeamNickName { get; set; }
+        public string? PossessionTeamFranchiseCurrentLogoUrl { get; set; }
     }
 
+    [UsedImplicitly]
     public class PlayStat
     {
         public int StatId { get; set; }
         public int Yards { get; set; }
-        public string PlayerName { get; set; }
-        public string TeamId { get; set; }
-        public string TeamAbbreviation { get; set; }
-        public string GsisPlayerId { get; set; }
+        public string? PlayerName { get; set; }
+        public string? TeamId { get; set; }
+        public string? TeamAbbreviation { get; set; }
+        public string? GsisPlayerId { get; set; }
     }
 
+    [UsedImplicitly]
     public class Play
     {
-        public string ClockTime { get; set; }
+        public string? ClockTime { get; set; }
         public int Down { get; set; }
-        public string EndClockTime { get; set; }
-        public string EndYardLine { get; set; }
+        public string? EndClockTime { get; set; }
+        public string? EndYardLine { get; set; }
         public bool FirstDown { get; set; }
         public bool GoalToGo { get; set; }
         public bool NextPlayIsGoalToGo { get; set; }
-        public string NextPlayType { get; set; }
+        public string? NextPlayType { get; set; }
         public double OrderSequence { get; set; }
         public bool PenaltyOnPlay { get; set; }
-        public int PlayClock { get; set; }
+        [JsonIgnore] public int PlayClock { get; set; }
         public bool PlayDeleted { get; set; }
-        public string PlayDescription { get; set; }
-        public string PlayDescriptionWithJerseyNumbers { get; set; }
+        public string? PlayDescription { get; set; }
+        public string? PlayDescriptionWithJerseyNumbers { get; set; }
         public int PlayId { get; set; }
-        public string PlayType { get; set; }
+        public string? PlayType { get; set; }
         public IList<PlayStat> PlayStats { get; set; }
-        public string PrePlayByPlay { get; set; }
+        public string? PrePlayByPlay { get; set; }
         public int Quarter { get; set; }
         public bool ScoringPlay { get; set; }
-        public string ShortDescription { get; set; }
+        public string? ShortDescription { get; set; }
         public bool SpecialTeamsPlay { get; set; }
-        public string TimeOfDay { get; set; }
-        public string YardLine { get; set; }
+        public string? TimeOfDay { get; set; }
+        public string? YardLine { get; set; }
         public int Yards { get; set; }
         public int YardsToGo { get; set; }
         public int? DriveNetYards { get; set; }
         public int? DrivePlayCount { get; set; }
         public int? DriveSequenceNumber { get; set; }
-        public string DriveTimeOfPossession { get; set; }
-        public string PossessionTeamAbbreviation { get; set; }
-        public string PossessionTeamNickName { get; set; }
-        public string PossessionTeamFranchiseCurrentLogoUrl { get; set; }
+        public string? DriveTimeOfPossession { get; set; }
+        public string? PossessionTeamAbbreviation { get; set; }
+        public string? PossessionTeamNickName { get; set; }
+        public string? PossessionTeamFranchiseCurrentLogoUrl { get; set; }
         public bool? IsBigPlay { get; set; }
-        public string ScoringPlayType { get; set; }
-        public string ScoringTeamId { get; set; }
-        public string ScoringTeamAbbreviation { get; set; }
-        public string ScoringTeamNickName { get; set; }
-        public string StPlayType { get; set; }
+        public string? ScoringPlayType { get; set; }
+        public string? ScoringTeamId { get; set; }
+        public string? ScoringTeamAbbreviation { get; set; }
+        public string? ScoringTeamNickName { get; set; }
+        public string? StPlayType { get; set; }
     }
 
     public class GameDetail
